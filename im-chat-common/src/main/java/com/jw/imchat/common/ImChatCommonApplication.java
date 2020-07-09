1e752017384b271275b4c3928819a32a1ec97742
JWangNov <wangjialun@126.com>
2020-07-09 11:38:42 -0400

    Initialize module im-chat-common

---
+++ im-chat-common/src/main/java/com/jw/imchat/common/ImChatCommonApplication.java
@@ -0,0 +1,13 @@
+package com.jw.imchat.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImChatCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImChatCommonApplication.class, args);
+    }
+
+}
---
+++ im-chat-common/src/test/java/com/jw/imchat/common/ImChatCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.imchat.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImChatCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
