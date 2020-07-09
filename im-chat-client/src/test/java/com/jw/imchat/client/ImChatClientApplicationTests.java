342a5ca3a4afa35b7e1dd08146db9ab793962187
JWangNov <wangjialun@126.com>
2020-07-09 11:36:01 -0400

    Initialize module im-chat-client

---
+++ im-chat-client/src/test/java/com/jw/imchat/client/ImChatClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.imchat.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImChatClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
