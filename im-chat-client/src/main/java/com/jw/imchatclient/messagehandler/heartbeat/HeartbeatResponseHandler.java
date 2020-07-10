7e703df9c2f089efb4e241920dc0182974264bfd
JWangNov <wangjialun@126.com>
2020-07-10 14:12:53 -0400

    Create HeartbeatResponseHandler

---
+++ im-chat-client/src/main/java/com/jw/imchatclient/messagehandler/heartbeat/HeartbeatResponseHandler.java
@@ -0,0 +1,22 @@
+package com.jw.imchatclient.messagehandler.heartbeat;
+
+import com.jw.imchatclient.message.heartbeat.HeartbeatResponse;
+import com.jw.imchatcommon.dispatcher.MessageHandler;
+import io.netty.channel.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class HeartbeatResponseHandler implements MessageHandler<HeartbeatResponse> {
+
+    @Override
+    public void execute(Channel channel, HeartbeatResponse message) {
+        log.info("[execute][receive heartbeat response from channel ({})]", channel.id());
+    }
+
+    @Override
+    public String getType() {
+        return HeartbeatResponse.TYPE;
+    }
+}
