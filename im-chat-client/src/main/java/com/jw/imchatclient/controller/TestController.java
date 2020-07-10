0a318de44465610c09f81597b81c432baabdb5fe
JWangNov <wangjialun@126.com>
2020-07-10 15:19:22 -0400

    Create TestController.java
    as you know, it's for testing..

---
+++ im-chat-client/src/main/java/com/jw/imchatclient/controller/TestController.java
@@ -0,0 +1,28 @@
+package com.jw.imchatclient.controller;
+
+import com.jw.imchatclient.client.ImChatClient;
+import com.jw.imchatcommon.codec.Invocation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * This is only for testing auth procedure now
+ * <p>
+ * e.g.: http://localhost:8080/test/simulate?type=AUTH_REQUEST&message={"accessToken":"jw"}
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private ImChatClient imChatClient;
+
+    @PostMapping("/simulate")
+    public String simulate(String type, String message) {
+        Invocation invocation = new Invocation(type, message);
+        imChatClient.send(invocation);
+        return "success";
+    }
+}
