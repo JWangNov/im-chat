c67680f63472271c8a1b551b88b26d41de71c78d
JWangNov <wangjialun@126.com>
2020-07-10 00:38:43 -0400

    Create ImChatClientHandlerInitializer

--- im-chat-client/src/main/java/com/jw/imchatclient/client/handler/ImChatClientHandlerInitializer.java
+++ im-chat-client/src/main/java/com/jw/imchatclient/client/handler/ImChatClientHandlerInitializer.java
@@ -1,37 +1,35 @@
-package com.jw.imchatserver.server.handler;
+package com.jw.imchatclient.client.handler;
 
 import com.jw.imchatcommon.codec.InvocationDecoder;
 import com.jw.imchatcommon.codec.InvocationEncoder;
 import com.jw.imchatcommon.dispatcher.MessageDispatcher;
-import com.jw.imchatserver.server.ImChatServerHandler;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
+import io.netty.handler.timeout.IdleStateHandler;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.TimeUnit;
-
 @Component
-public class ImChatServerHandlerInitializer extends ChannelInitializer<Channel> {
+public class ImChatClientHandlerInitializer extends ChannelInitializer<Channel> {
 
     private static final Integer READ_TIMEOUT_SECONDS = 3 * 60;
+    private static final Integer READER_IDLE_TIME_SECONDS = 60;
 
     @Autowired
     private MessageDispatcher messageDispatcher;
     @Autowired
-    private ImChatServerHandler imChatServerHandler;
+    private ImChatClientHandler imChatClientHandler;
 
     @Override
-    protected void initChannel(Channel channel) {
-        ChannelPipeline pipeline = channel.pipeline();
-        pipeline
-                .addLast(new ReadTimeoutHandler(READ_TIMEOUT_SECONDS, TimeUnit.SECONDS))
+    protected void initChannel(Channel channel) throws Exception {
+        channel.pipeline()
+                .addLast(new IdleStateHandler(READER_IDLE_TIME_SECONDS, 0, 0))
+                .addLast(new ReadTimeoutHandler(READ_TIMEOUT_SECONDS))
                 .addLast(new InvocationEncoder())
                 .addLast(new InvocationDecoder())
                 .addLast(messageDispatcher)
-                .addLast(imChatServerHandler)
+                .addLast(imChatClientHandler)
         ;
     }
 }
