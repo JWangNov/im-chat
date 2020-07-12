748d66b3955a71e7ede4c95cc420ca2c197e5204
JWangNov <wangjialun8421@gmail.com>
2020-07-12 15:22:11 -0400

    Create ChatSendToAllHandler
    for server

---
+++ im-chat-server/src/main/java/com/jw/imchatserver/messagehandler/chat/ChatSendToAllHandler.java
@@ -0,0 +1,38 @@
+package com.jw.imchatserver.messagehandler.chat;
+
+import com.jw.imchatcommon.codec.Invocation;
+import com.jw.imchatcommon.dispatcher.MessageHandler;
+import com.jw.imchatserver.message.chat.ChatRedirectToUserRequest;
+import com.jw.imchatserver.message.chat.ChatSendResponse;
+import com.jw.imchatserver.message.chat.ChatSendToAllRequest;
+import com.jw.imchatserver.server.handler.ImChatChannelManager;
+import io.netty.channel.Channel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ChatSendToAllHandler implements MessageHandler<ChatSendToAllRequest> {
+
+    @Autowired
+    private ImChatChannelManager channelManager;
+
+    @Override
+    public void execute(Channel channel, ChatSendToAllRequest message) {
+        ChatSendResponse sendResponse = new ChatSendResponse()
+                .setMsgId(message.getMsgId())
+                .setCode(0);
+        channel.writeAndFlush(new Invocation(ChatSendResponse.TYPE, sendResponse));
+
+        ChatRedirectToUserRequest redirectToUserRequest = new ChatRedirectToUserRequest()
+                .setMsgId(message.getMsgId())
+                .setContent(message.getContent());
+        channelManager.sendAll(
+                new Invocation(ChatRedirectToUserRequest.TYPE, redirectToUserRequest)
+        );
+    }
+
+    @Override
+    public String getType() {
+        return ChatSendToAllRequest.TYPE;
+    }
+}
