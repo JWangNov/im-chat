77be99c0df5a4ba632da64822a8d56e0afea1e78
JWangNov <wangjialun8421@gmail.com>
2020-07-12 15:10:10 -0400

    Create ChatSendToAllRequest
    for client

---
+++ im-chat-client/src/main/java/com/jw/imchatclient/message/chat/ChatSendToAllRequest.java
@@ -0,0 +1,40 @@
+package com.jw.imchatclient.message.chat;
+
+import com.jw.imchatcommon.dispatcher.Message;
+import lombok.Getter;
+
+/**
+ * This is to send message to all client
+ * <p>
+ * todo: for group chat, need to add groupId based on this class, also add groupId field into all clients
+ */
+@Getter
+public class ChatSendToAllRequest implements Message {
+
+    public static final String TYPE = "CHAT_SEND_TO_ALL_REQUEST";
+
+    private String msgId;
+    private String content;
+
+    // --- special setters for streaming
+
+    public ChatSendToAllRequest setMsgId(String msgId) {
+        this.msgId = msgId;
+        return this;
+    }
+
+    public ChatSendToAllRequest setContent(String content) {
+        this.content = content;
+        return this;
+    }
+
+    // --- special toString to fit json format
+
+    @Override
+    public String toString() {
+        return "ChatSendToAllRequest{" +
+                "msgId='" + msgId + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
