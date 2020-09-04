62808ce2de71a1381e7226d66f1824353feb8f57
JWangNov <wangjialun8421@gmail.com>
2020-09-04 13:21:53 -0400

    Update Message

--- im-chat-common/src/main/java/com/jw/imchatcommon/message/Message.java
+++ im-chat-common/src/main/java/com/jw/imchatcommon/message/Message.java
@@ -2,9 +2,5 @@ package com.jw.imchatcommon.message;
 
 public interface Message {
     // leave this empty, just for further implement based on this
-
-    // To implement this, the class needs to have:
-    // --- special toString to fit json format
-    // Also, its setters should allow chaining
-    // --- can use Lombok @Accessors(chain = true)
+    // method toString() should fit json format
 }
